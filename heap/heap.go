1f0abebb1ff932a838eec76d22a172bcd2877e53
Bao Nguyen <b@nqbao.com>
2018-09-13 12:36:54 -0700

    Heap sort

--- heap/heap.go
+++ heap/heap.go
@@ -6,6 +6,16 @@ func VerifyHeap(heap []int) bool {
 	return verifyHeapNode(heap, 0)
 }
 
+// Sort sorts an array using HeapSort algorithm
+func Sort(heap []int) {
+	for size := len(heap); size > 1; size-- {
+		heapify(heap, size)
+
+		// move the first to the last
+		heap[size-1], heap[0] = heap[0], heap[size-1]
+	}
+}
+
 func verifyHeapNode(heap []int, node int) bool {
 	leftChild := node * 2
 	rightChild := node*2 + 1
@@ -25,14 +35,17 @@ func verifyHeapNode(heap []int, node int) bool {
 // Heapify converts an array to ensure it satisfies the heap property
 // this function performs inplace edit of the given array
 func Heapify(heap []int) {
+	heapify(heap, len(heap))
+}
+
+func heapify(heap []int, size int) {
 	// convert all subtrees of the heaps
 	for i := len(heap) / 2; i >= 0; i-- {
-		heapifySubtree(heap, i)
+		heapifySubtree(heap, i, size)
 	}
 }
 
-func heapifySubtree(heap []int, node int) {
-	size := len(heap)
+func heapifySubtree(heap []int, node int, size int) {
 	left := 2*node + 1
 	right := 2*node + 2
 
@@ -49,6 +62,6 @@ func heapifySubtree(heap []int, node int) {
 	// move down
 	if largest != node {
 		heap[largest], heap[node] = heap[node], heap[largest]
-		heapifySubtree(heap, largest)
+		heapifySubtree(heap, largest, size)
 	}
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -41,13 +41,40 @@ func TestHeapify(t *testing.T) {
 
 	for _, test := range tables {
 		original := make([]int, len(test))
-		copy(test, original)
+		copy(original, test)
 		heap.Heapify(test)
 
 		t.Logf("Result %v", test)
 
 		if !heap.VerifyHeap(test) {
 			t.Errorf("Unable to heapify %v", original)
+		}
+	}
+}
+
+func TestHeapSort(t *testing.T) {
+	tables := [][]int{
+		[]int{},
+		[]int{5},
+		[]int{10, 20},
+		[]int{10, 20, 15},
+		[]int{4, 7, 8, 3, 2, 6, 5},
+		[]int{1, 4, 3, 7, 8, 9, 10},
+		[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 100},
+	}
+
+	for _, test := range tables {
+		original := make([]int, len(test))
+		copy(original, test)
+		heap.Sort(test)
+
+		t.Logf("Result %v", test)
+
+		// verify if this is ascending
+		for i := 0; i < len(test)-1; i++ {
+			if test[i] > test[i+1] {
+				t.Errorf("Unable to sort %v, result: %v", original, test)
+			}
 		}
 	}
 }
