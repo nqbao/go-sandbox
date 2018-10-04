eb5028dbdc59e3434082cd1a9b81fc306eb85f94
Bao Nguyen <b@nqbao.com>
2018-10-03 22:58:49 -0700

    try hashCode example

--- jni/main.go
+++ jni/main.go
@@ -15,9 +15,7 @@ import (
 	"github.com/juntaki/jnigo"
 )
 
-func main() {
-	vm := jnigo.CreateJVM()
-
+func getJavaVersion(vm *jnigo.JVM) string {
 	args := []jnigo.JObject{}
 
 	if str, e := vm.NewString("java.version"); e == nil {
@@ -30,5 +28,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%v", v.GoValue())
+	return fmt.Sprintf("%v", v.GoValue())
+}
+
+func testHashcode(vm *jnigo.JVM) int32 {
+	// simulate the example from https://github.com/timob/jnigi#example
+	obj, err := vm.NewJClass("java/lang/Object", []jnigo.JObject{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	v, err := obj.CallFunction("hashCode", "()I", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return v.GoValue().(int32)
+}
+
+func main() {
+	vm := jnigo.CreateJVM()
+
+	fmt.Printf("Java version: %v\n", getJavaVersion(vm))
+	fmt.Printf("Object hash code: %v\n", testHashcode(vm))
 }
