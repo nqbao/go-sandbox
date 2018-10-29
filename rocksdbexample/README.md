85317702e6de02ded5e92d2a8a1bc14269bdb875
Bao Nguyen <b@nqbao.com>
2018-10-28 17:23:30 -0700

    Column db example

--- rocksdbexample/README.md
+++ rocksdbexample/README.md
@@ -14,3 +14,12 @@ export CGO_LDFLAGS="-L$ROCKSDB_PATH -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -l
 export LD_LIBRARY_PATH="$ROCKSDB_PATH:$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH="$ROCKSDB_PATH:$DYLD_LIBRARY_PATH"
 ```
+
+Remarks:
+
+  * Interface is quite verbose, not as clean as BoltDB
+  * You need to create Read/Write options. Make sure to free each of them.
+  * There is no Go document, so you need to read Go code or [the document for C++](https://github.com/facebook/rocksdb/wiki)
+  * Iterator is similar to Cursor in BoltDB
+  * Column Familiy are like Buckets in BoltDB
+    * You need to make sure to specify column families when you open the DB. Or you need to open ALL column families.
--- rocksdbexample/main.go
+++ rocksdbexample/main.go
@@ -6,25 +6,69 @@ import (
 	rocksdb "github.com/tecbot/gorocksdb"
 )
 
-func main() {
+func OpenAllCFs(name string) (*rocksdb.DB, []*rocksdb.ColumnFamilyHandle, error) {
 	opts := rocksdb.NewDefaultOptions()
 	defer opts.Destroy()
 	opts.SetCreateIfMissing(true)
 
 	// how many LOG.old file to keep
 	opts.SetKeepLogFileNum(10)
+	opts.SetCompression(rocksdb.ZLibCompression)
 
-	db, err := rocksdb.OpenDb(opts, "./db")
-	defer db.Close()
+	cfs, err := rocksdb.ListColumnFamilies(opts, "./db")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cfOpts := make([]*rocksdb.Options, len(cfs))
+	for i := range cfs {
+		cfOpt := rocksdb.NewDefaultOptions()
+		cfOpt.SetKeepLogFileNum(10)
+		cfOpt.SetCompression(rocksdb.ZLibCompression)
+
+		cfOpts[i] = cfOpt
+	}
+
+	// free all the options for cfs
+	defer func() {
+		for i := range cfOpts {
+			cfOpts[i].Destroy()
+		}
+	}()
+
+	fmt.Printf("column families: %v\n", cfs)
+
+	return rocksdb.OpenDbColumnFamilies(opts, "./db", cfs, cfOpts)
+}
+
+func main() {
+	db, cfs, err := OpenAllCFs("./db")
 
 	if err != nil {
 		panic(err)
 	}
 
+	defer func() {
+		for _, cf := range cfs {
+			cf.Destroy()
+		}
+		db.Close()
+	}()
+
 	// write a key
 	writeOpts := rocksdb.NewDefaultWriteOptions()
 	defer writeOpts.Destroy()
 	err = db.Put(writeOpts, []byte("hello"), []byte("world"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// write multiple keys at a time
+	batch := rocksdb.NewWriteBatch()
+	batch.Put([]byte("test"), []byte("value"))
+	batch.Put([]byte("test2"), []byte("value2"))
+	err = db.Write(writeOpts, batch)
 
 	if err != nil {
 		panic(err)
@@ -42,4 +86,24 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", result.Data())
+
+	// loop through all keys
+	it := db.NewIterator(readOpts)
+	defer it.Close()
+	it.SeekToFirst()
+
+	for ; it.Valid(); it.Next() {
+		fmt.Printf("Key: %s\n", it.Key().Data())
+	}
+
+	// column families
+	// cfOpts := rocksdb.NewDefaultOptions()
+	// defer cfOpts.Destroy()
+	// cf, err := db.CreateColumnFamily(cfOpts, "cf1")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer cf.Destroy()
+
+	// ListColumnFamilies
 }
